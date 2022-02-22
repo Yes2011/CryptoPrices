a5ff6a71f2634a3e175766280ca33bb5a6ae764b
Crispin Lingford <crispinlingford2011@gmail.com>
2022-02-22 16:28:35 +0000

    Initial Commit

---
+++ Shared/CryptoCheckerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoCheckerApp.swift
+//  Shared
+//
+//  Created by YES 2011 Limited on 22/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct CryptoCheckerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
