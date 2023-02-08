7039277c9020a29d519ed57e941c9b349aee9984
Crispin <office@yes2011.com>
2023-02-08 13:14:46 +0000

    Inject Font value into View

--- Views/CoinPriceView.swift
+++ Views/CoinPriceView.swift
@@ -10,16 +10,17 @@ import SwiftUI
 struct CoinPriceView: View {
 
     let value: Double?
+    let font: Font
     @AppStorage(AppStorageKey.currency) private var userCurrency: Currency = .usd
 
     var body: some View {
         Text("\(userCurrency.symbol)\(String(format: "%.2f", value ?? 0))")
-            .font(.body.weight(.regular))
+            .font(font)
     }
 }
 
 struct CoinPriceView_Previews: PreviewProvider {
     static var previews: some View {
-        CoinPriceView(value: 123.1)
+        CoinPriceView(value: 123.1, font: .body.weight(.regular))
     }
 }
